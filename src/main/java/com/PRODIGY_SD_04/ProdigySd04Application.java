d733d4884e818744eb034da43123c991b1ef7c2d
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-09 14:10:24 +0530

    Initial commit, Project created from start.spring.io with these config java 17, JAR, Maven, Dep: Spring web, Devtools, Lombok, thymeleaf.

---
+++ src/main/java/com/PRODIGY_SD_04/ProdigySd04Application.java
@@ -0,0 +1,13 @@
+package com.PRODIGY_SD_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdigySd04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdigySd04Application.class, args);
+	}
+
+}
