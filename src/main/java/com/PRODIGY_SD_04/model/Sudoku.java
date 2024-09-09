29060a47b63e90dffba91329979ce15131138126
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-09 14:16:57 +0530

    model class implemented.

--- src/main/java/com/PRODIGY_SD_04/model/Sudoku.java
+++ src/main/java/com/PRODIGY_SD_04/model/Sudoku.java
@@ -1,4 +1,21 @@
 package com.PRODIGY_SD_04.model;
 
 public class Sudoku {
+    private int[][] grid;
+
+    public Sudoku() {
+        this.grid = new int[9][9]; // Initialize a 9x9 grid
+    }
+
+    public Sudoku(int[][] grid) {
+        this.grid = grid;
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(int[][] grid) {
+        this.grid = grid;
+    }
 }
