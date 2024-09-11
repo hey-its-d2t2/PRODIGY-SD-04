18560a13feb06da5edb9ec452ba1669b0940291b
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-11 17:48:21 +0530

    Controller updated.

--- src/main/java/com/PRODIGY_SD_04/controller/SudokuController.java
+++ src/main/java/com/PRODIGY_SD_04/controller/SudokuController.java
@@ -5,26 +5,31 @@ import com.PRODIGY_SD_04.service.SudokuSolverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequestMapping("/")
 public class SudokuController {
 
     @Autowired
-    private SudokuSolverService sudokuSolverService;
+    private SudokuSolverService sudokuService;
 
-    @GetMapping
-    public String index(Model model) {
-        model.addAttribute("sudoku", new Sudoku());
+    @GetMapping("/")
+    public String home(Model model) {
+        Sudoku sudoku = new Sudoku();
+        sudoku.initializeEmptyGrid();
+        model.addAttribute("sudoku", sudoku);
         return "sudoku";
     }
 
     @PostMapping("/solve")
-    public String solveSudoku(@ModelAttribute Sudoku sudoku, Model model) {
-        boolean solved = sudokuSolverService.solveSudoku(sudoku.getGrid());
-        model.addAttribute("solved", solved);
+    public String solve(@ModelAttribute("sudoku") Sudoku sudoku, Model model) {
+        int[][] grid = sudoku.getGrid();
+        boolean solved = sudokuService.solveSudoku(grid);
+        sudoku.setGrid(grid);
         model.addAttribute("sudoku", sudoku);
+        model.addAttribute("solved", solved);
         return "sudoku";
     }
-}   
+}
